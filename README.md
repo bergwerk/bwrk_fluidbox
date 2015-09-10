36b6979862940e2197978cee41d2961a5f70cfd5
Georg Dümmler <gd@bergwerk.ag>
2015-09-10 13:38:05 +0200

    Add Readme, Versions and Copyright

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fluidbox
+
+## Installation
+1. Install the extension
+2. Include the extension-template in your TYPO3
+
+## How to use
+You only need to install the extension like the Installation-Guide.
+The Fluidbox is activated automatically if you active the checkbox "enlarge on click" at any Image-Content-Element
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,30 +1,52 @@
 <?php
 
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Georg Dümmler <gd@bergwerk.ag>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ *
+ * @author	Georg Dümmler <gd@bergwerk.ag>
+ * @package	TYPO3
+ * @subpackage	bwrk_fluidbox
+ ***************************************************************/
+
 $EM_CONF[$_EXTKEY] = array(
-   'title' => 'fluidbox',
-   'description' => '',
-   'category' => 'fe',
-   'author' => 'BERGWERK [DV]',
-   'author_email' => 'dv@bergwerk.ag',
-   'author_company' => 'BERGWERK',
-   'shy' => 0,
-   'priority' => 'top',
-   'module' => '',
-   'state' => 'stable',
-   'internal' => 0,
-   'uploadfolder' => '0',
-   'createDirs' => '',
-   'modify_tables' => '',
-   'clearCacheOnLoad' => 0,
-   'lockType' => '',
-   'version' => '1.0.0',
-   'constraints' => array(
-      'depends' => array(
-         'extbase' => '6.0',
-         'fluid' => '6.0',
-         'typo3' => '6.0',
-      ),
-      'conflicts' => array(),
-      'suggests' => array(),
-   ),
+    'title' => 'Fluidbox',
+    'description' => 'This extension adds the Fluidbox from Terry Mun to your TYPO3',
+    'category' => 'fe',
+    'author' => 'BERGWERK [DV], BERGWERK [GD]',
+    'author_email' => 'dv@bergwerk.ag, gd@bergwerk.ag',
+    'author_company' => 'BERGWERK',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.1.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-6.2.99',
+        ),
+        'conflicts' => array(
+            'bwrk_monsterkill' => '3.3.0'
+        ),
+        'suggests' => array(),
+    ),
 );
